3c806fca76dcba841fc08e23fb7c593221d89821
Guilherme Alberto <gmightasf@gmail.com>
2025-06-14 14:00:36 -0300

    update 2

---
+++ javascript/IA.js
@@ -0,0 +1,5 @@
+function ENS_IA(){
+    let pergunta = prompt("A ENS+IA está aqui para sanar as suas dúvidas! Fique à vontade para perguntar! :)")
+    window.location = "../paginas/cadastro.html"
+    window.alert("Cadastre-se para usar a IA! ")
+}
